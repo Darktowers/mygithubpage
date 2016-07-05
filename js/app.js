7315f5f475bb4f184d98bf736a969c9fd351e4c4
Cristian Arrieta <darktowerdev@gmail.com>
2016-07-05 17:24:06 -0500

    Mejorando El Diseño

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-alert("Hello");
+function cambios(e){
+    if(!$("#developer").hasClass(e+'Font')){
+        $("#developer").removeClass() 
+        $("#developer").addClass(e+'Font');
+    }
+    if(!$("#code").hasClass(e+'Font')){
+        $("#code").removeClass()
+        $("#code").addClass('icon-code'); 
+        $("#code").addClass(e+'Font');
+        
+    }
+    if(!$("#aboutShort").hasClass(e+'Font')){
+        $("#aboutShort").removeClass() 
+        $("#aboutShort").addClass(e+'Font');
+    }
+    if(!$("#back").hasClass(e)){
+        $("#back").removeClass() 
+        $("#back").addClass(e);
+    }
+    if(!$("#menuc").hasClass(e+'Line')){
+        $("#menuc").removeClass() 
+        $("#menuc").addClass(e+'Line');
+    }
+        
+}
+
+$(".tab").on("click",function(){
+    cambios($(this).attr("val"));
+});
